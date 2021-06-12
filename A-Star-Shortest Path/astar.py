b28b754b42bdf53e712cef90c865e7ee386edd57
111989 <6uptaaashi@gmail.com>
2021-06-12 19:27:00 -0400

    Update astar.py

--- A-Star-Shortest Path/astar.py
+++ A-Star-Shortest Path/astar.py
@@ -1,8 +1,7 @@
 """
     A* Shortest Path Finding 
     Algorithm implementation 
-    and interactive visualization
-"""
+    and interactive visualization"""
 
 import pygame
 import argparse
@@ -54,8 +53,8 @@ def algorithm(display, display_width: int, display_height: int, \
     def heuristic(cell):
         """
             Returns Manhattan Distance
-            between the input cell and end_cell
-        """
+            between the input cell and end_cell"""
+        
         x1, y1 = cell.get_cell_position()
         x2, y2 = end_cell.get_cell_position()
         return abs(x2-x1) + abs(y2-y1)
@@ -63,8 +62,8 @@ def algorithm(display, display_width: int, display_height: int, \
         Initialize a Priority Queue 'open_cells'
         for the algorithm to track cells with
         low f-scores, and 'tie_breaker' to break ties 
-        between cells with the same f-score.
-    """
+        between cells with the same f-score"""
+    
     tie_breaker = 0 
     start_cell.g_score = 0
     start_cell.f_score = heuristic(start_cell)
@@ -77,13 +76,13 @@ def algorithm(display, display_width: int, display_height: int, \
             if event.type == pygame.QUIT:
                 pygame.quit()
 
-        #fetch the cell with the least f_score, call it current_cell
+        # fetch the cell with the least f_score, call it current_cell
         current_cell = open_cells.get()[2] 
         open_cells_set.remove(current_cell)
 
         if current_cell == end_cell:
             
-            #draw optimal path
+            # draw optimal path
             temp_cell = end_cell
             while temp_cell.parent != start_cell:
                 temp_cell = temp_cell.parent
@@ -91,39 +90,39 @@ def algorithm(display, display_width: int, display_height: int, \
                 draw(display, display_width, display_height, matrix, total_cells, NONE)
             start_cell.mode, end_cell.mode = START, END
             
-            #optimal path found, terminate algorithm
+            # optimal path found, terminate algorithm
             break
 
-        #generate successors of the current_cell
-        #TOP
+        # generate successors of the current_cell
+        # TOP
         if current_cell.cell_row > 0 and \
             matrix[current_cell.cell_row-1][current_cell.cell_col].mode != OBSTACLE:
             current_cell.successors.append(matrix[current_cell.cell_row-1][current_cell.cell_col])
-        #RIGHT
+        # RIGHT
         if current_cell.cell_col < current_cell.total_cells-1 and \
             matrix[current_cell.cell_row][current_cell.cell_col+1].mode != OBSTACLE:
             current_cell.successors.append(matrix[current_cell.cell_row][current_cell.cell_col+1])
-        #BOTTOM
+        # BOTTOM
         if current_cell.cell_row < current_cell.total_cells-1 and \
             matrix[current_cell.cell_row+1][current_cell.cell_col].mode != OBSTACLE: 
             current_cell.successors.append(matrix[current_cell.cell_row+1][current_cell.cell_col])
-        #LEFT
+        # LEFT
         if current_cell.cell_col > 0 and \
             matrix[current_cell.cell_row][current_cell.cell_col-1].mode != OBSTACLE:
             current_cell.successors.append(matrix[current_cell.cell_row][current_cell.cell_col-1])
-        # #TOP-LEFT
+        # # TOP-LEFT
         # if current_cell.cell_row > 0 and current_cell.cell_col > 0 and \
         #     matrix[current_cell.cell_row-1][current_cell.cell_col-1] != OBSTACLE:
         #     current_cell.successors.append(matrix[current_cell.cell_row-1][current_cell.cell_col-1])
-        # #TOP-RIGHT
+        # # TOP-RIGHT
         # if current_cell.cell_row > 0 and current_cell.cell_col < current_cell.total_cells-1 and \
         #     matrix[current_cell.cell_row-1][current_cell.cell_col+1] != OBSTACLE:
         #     current_cell.successors.append(matrix[current_cell.cell_row-1][current_cell.cell_col+1])
-        # #BOTTOM-RIGHT
+        # # BOTTOM-RIGHT
         # if current_cell.cell_row < current_cell.total_cells-1 and current_cell.cell_col < current_cell.total_cells-1 and \
         #     matrix[current_cell.cell_row+1][current_cell.cell_col+1] != OBSTACLE:
         #     current_cell.successors.append(matrix[current_cell.cell_row+1][current_cell.cell_col+1])
-        # #BOTTOM-LEFT
+        # # BOTTOM-LEFT
         # if current_cell.cell_row < current_cell.total_cells-1 and current_cell.cell_col > 0 and \
         #     matrix[current_cell.cell_row+1][current_cell.cell_col-1] != OBSTACLE:
         #     current_cell.successors.append(matrix[current_cell.cell_row+1][current_cell.cell_col-1])
@@ -149,7 +148,7 @@ def algorithm(display, display_width: int, display_height: int, \
 
 def main():
     
-    #initialize cell modes
+    # initialize cell modes
     NONE = (50, 50, 50) 
     START = (255, 255, 0) 
     END = (0, 255, 255)
@@ -158,11 +157,11 @@ def main():
     CLOSED = (255, 128, 128)
     PATH = (128, 225, 0)
 
-    #initialize pygame display
+    # initialize pygame display
     display = pygame.display.set_mode((display_width, display_height)) 
     pygame.display.set_caption("A* Shortest Path Finding Algorithm")
     
-    #generate matrix
+    # generate matrix
     matrix = []
     for cell_row in range(total_cells):
         matrix.append([])
@@ -180,7 +179,7 @@ def main():
 
             if pygame.mouse.get_pressed()[0]: #left mouse click
                 
-                #get cell position
+                # get cell position
                 cell_width = display_width // total_cells
                 cell_height = display_height // total_cells
                 cell_row = pygame.mouse.get_pos()[0] // cell_width 
@@ -202,13 +201,13 @@ def main():
                         total_cells, start_cell, end_cell, NONE, START, END, \
                             OBSTACLE, OPEN, CLOSED, PATH)
                 
-            #update display 
+            # update display 
         draw(display, display_width, display_height, matrix, total_cells, NONE)
     pygame.quit()
     
 if __name__ == '__main__':
     
-    #parse input arguments
+    # parse input arguments
     parser = argparse.ArgumentParser()
     
     parser.add_argument('--width', type = int, default = 1200, \
