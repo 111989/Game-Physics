6e9441d3d9a03728086f36126484f542bad9448f
111989 <6uptaaashi@gmail.com>
2021-06-16 09:09:51 -0400

    Update gjk_main.py

--- GJK-Collision-Detection/gjk_main.py
+++ GJK-Collision-Detection/gjk_main.py
@@ -2,17 +2,17 @@ import os
 import pygame
 from gjk_algorithm import intersect
 
+
 def main():
+    
     #initialize colours
+    #initialize the fixed polygon
+    #initialize pygame display
     BACKGROUND = (175, 175, 225)
     POLYGON = (225, 225, 225)
     RED = (200, 75, 75)
     GREEN = (75, 200, 75)
-
-    #initialize the fixed polygon
     polygon1 = [[300, 500], [500, 300], [240, 240]]
-
-    #initialize pygame display
     DISPLAY_WIDTH = 1000
     DISPLAY_HEIGHT = 600
     DISPLAY = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
@@ -28,24 +28,19 @@ def main():
         
         
         #fill display
+        #draw the fixed polygon
+        #get the position of polygon2
+        #run the GJK algorithm
+        #draw polygon2
         pygame.time.delay(10)
         pygame.display.flip()
         DISPLAY.fill(BACKGROUND)
-
-        #draw the fixed polygon
         pygame.draw.polygon(DISPLAY, POLYGON, polygon1)
-
-        #get the position of polygon2
         position = pygame.mouse.get_pos()
-        polygon2 = [[50+position[0], 50+position[1]], \
-            [50+position[0], -15+position[1]], \
-                [position[0], -50+position[1]], \
-                    [-60+position[0], position[1]]]
-
-        #run the GJK algorithm
-        collision = intersect(polygon1, polygon2)
-
-        #draw polygon2
+        polygon2 = [[50+position[0], 50+position[1]], [50+position[0], -15+position[1]], 
+            [position[0], -50+position[1]], [-60+position[0], position[1]]]
+        
+        collision = intersect(polygon1, polygon2)        
         pygame.draw.polygon(DISPLAY, RED if collision else GREEN, polygon2)
 
 
